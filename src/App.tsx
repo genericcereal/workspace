9c2e29257d2fe10cf6b4567a8fc61c0801c51fdd
Jay Goggin <jgoggin1@gmail.com>
2024-06-11 01:28:15 +0000

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and sav
+          Edit <code>src/App.tsx</code>
         </p>
       </div>
       <p className="read-the-docs">
